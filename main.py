5b4b3f040ec758583a5f66b4cd2f67b1af2a9f00
Kirill Neevin <neevin@yandex-team.ru>
2023-07-13 17:01:50 +0300

    refactored

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import math
 import time
-
 from prometheus_client import start_http_server, Gauge
+
+
+def track_temperature(temperature):
+    x = 0
+    while True:
+        temperature.labels(place='garden').set(10 * math.cos(x))
+        x += 0.1
+        time.sleep(3)
 
 
 if __name__ == '__main__':
     start_http_server(8000)
-    temperature = Gauge('air_temperature', 'Air temperature', labelnames=['place'])
-    x = 0
-
-    while True:
-        temperature.labels(place='garden').set(
-            10 * math.cos(x)
-        )
-
-        x += 0.1
-        time.sleep(3)
+    temperature_metric = Gauge('air_temperature', 'Air temperature', labelnames=['place'])
+    track_temperature(temperature_metric)
